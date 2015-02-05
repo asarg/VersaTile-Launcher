3f2a8417fcc97b84bfd9e2fd4325f74f4b391c1a
Eric Martinez <eric.michael.mtz@gmail.com>
2015-02-05 01:22:46 -0600

    Move updating to background task

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import javafx.application.Application;
+import javafx.concurrent.Task;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -9,31 +10,41 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/VersaTileLauncher.fxml"));
-        primaryStage.setTitle("Welcome to VersaTile");
-        primaryStage.setResizable(false);
-        primaryStage.setScene(new Scene(root, 707, 517));
-        primaryStage.show();
+        Version v = AutoUpdate.getVersion();
+        System.out.println("Installed " + v.toString());
+
+        if(v!=null) {
+            Task<Void> updater = new Task<Void>() {
+                @Override
+                protected Void call() throws Exception {
+                    Release newRelease = AutoUpdate.checkForUpdates();
+                    if(newRelease!=null){
+                        System.out.println("New Release Available");
+                        Downloader downloader = new Downloader(newRelease);
+                        downloader.call();
+                    }
+                    return null;
+                }
+            };
+
+            new Thread(updater).start();
+
+            try {
+                AutoUpdate.startLatest(null);
+            }catch(Exception e){
+                System.out.println(e.getMessage());
+                e.printStackTrace();
+            }
+        }else {
+            Parent root = FXMLLoader.load(getClass().getResource("/VersaTileLauncher.fxml"));
+            primaryStage.setTitle("Welcome to VersaTile");
+            primaryStage.setResizable(false);
+            primaryStage.setScene(new Scene(root, 707, 517));
+            primaryStage.show();
+        }
     }
 
     public static void main(String[] args) {
-        Version v = AutoUpdate.getVersion();
-        if(v!=null) {
-            System.out.println("Installed " + v.toString());
-            Release newRelease = AutoUpdate.checkForUpdates();
-            if(newRelease!=null){
-                System.out.println("New Release Available");
-            }else {
-                System.out.println("Up to date");
-                try {
-                    AutoUpdate.launchLatest(args);
-                }catch(Exception e){
-                    System.out.println(e.getMessage());
-                    e.printStackTrace();
-                }
-            }
-        }else {
-            launch(args);
-        }
+        launch(args);
     }
 }
